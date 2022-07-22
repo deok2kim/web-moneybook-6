51f8169fbd0acf56958d8039fe668c012a8ff76c
deokgi kim <dukgi77@naver.com>
2022-07-22 13:45:12 +0900

    Refactor: :hammer: 라우터 모듈로 분리

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,54 +1,21 @@
 import Component from '@/utils/Component';
-import MainPage from '@/pages/MainPage';
-import CalendarPage from '@/pages/CalendarPage';
-import StatisticsPage from '@/pages/StatisticsPage';
 import Header from '@/components/Header';
 import '@/assets/styles/app.scss';
+
+import { route } from '@/router';
 
 class App extends Component {
   template() {
     return `
-      <header>
-        <span>WEB-MONEYBOOK-6</span>
-        <a id="main" href="/">메인</a>
-        <a id="calendar" href="/calendar">달력</a>
-        <a id="statistics" href="/statistics">통계</a>
-      </header>
+      <header></header>
       <div class="body"></div>
     `;
   }
 
-  setEvent() {
-    this.$target.addEventListener('click', (e) => {
-      e.preventDefault();
-      if (e.target.tagName === 'A') {
-        const { pathname } = e.target;
-        window.history.pushState({}, {}, pathname);
-        this.route();
-      }
-    });
-  }
-
-  route() {
-    const $body = document.querySelector('.body');
-    $body.innerHTML = '';
-
-    const { pathname } = location;
-    if (pathname === '/') {
-      new MainPage($body);
-    } else if (pathname === '/calendar') {
-      new CalendarPage($body);
-    } else if (pathname === '/statistics') {
-      new StatisticsPage($body);
-    } else {
-      $body.innerHTML = '<h1>404 NOT FOUND</h1>';
-    }
-  }
-
   init() {
     new Header(document.querySelector('header'));
-    this.route();
-    window.addEventListener('popstate', () => this.route());
+    route();
+    window.addEventListener('popstate', () => route());
   }
 }
 
--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -6,9 +6,11 @@ import 통계 from '@/assets/images/통계.svg';
 import 달력 from '@/assets/images/달력.svg';
 import 내역 from '@/assets/images/내역.svg';
 
+import { route } from '@/router';
+
 export default class Header extends Component {
   template() {
-    return `
+    return /*html*/ `
       <div class="logo">우아한 가계부</div>
       <div class="month-year">
         <img src=${arrowLeft} />
@@ -19,10 +21,20 @@ export default class Header extends Component {
         <img src=${arrowRight} />
       </div>
       <div class="tab">
-        <img src=${내역} />
-        <img src=${달력} />
-        <img src=${통계} />
+        <a href=""><img src=${내역} data-url="/"/></a>
+        <a href=""><img src=${달력} data-url="/calendar"/></a>
+        <a href=""><img src=${통계} data-url="/statistics"/></a>
       </div>
     `;
+  }
+  setEvent() {
+    this.$target.addEventListener('click', (e) => {
+      e.preventDefault();
+      if (e.target.tagName === 'IMG') {
+        const { url } = e.target.dataset;
+        window.history.pushState({}, {}, url);
+        route();
+      }
+    });
   }
 }
