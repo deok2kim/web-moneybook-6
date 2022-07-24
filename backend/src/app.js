07e01828ff266ea097b8cada1e21d52b0cc1d86d
deokgi kim <dukgi77@naver.com>
2022-07-24 16:45:37 +0900

    Feat: :sparkles: CORS 적용

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,19 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = 5001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
-const moneyBookRouter = require('./router/moneyBook');
-const paymentMethodRouter = require('./router/paymentMethod');
-const categoryRouter = require('./router/category');
+const moneyBookRouter = require("./router/moneyBook");
+const paymentMethodRouter = require("./router/paymentMethod");
+const categoryRouter = require("./router/category");
 
-app.use('/api/money_book', moneyBookRouter);
-app.use('/api/payment_method', paymentMethodRouter);
-app.use('/api/category', categoryRouter);
+app.use("/api/money_book", moneyBookRouter);
+app.use("/api/payment_method", paymentMethodRouter);
+app.use("/api/category", categoryRouter);
 
 app.listen(PORT, () => {
   console.log(`SERVER START: PORT-${PORT}`);
